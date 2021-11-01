a4c6a297d767b5007d9637b75121471c6bb737bf
DarkAthena <darkathena@qq.com>
2021-11-01 22:57:45 +0800

    Update pl4py_func_list_v.sql

--- pl4py_func_list_v.sql
+++ pl4py_func_list_v.sql
@@ -2,7 +2,8 @@ create or replace view pl4py_func_list_v as
 select
        A.func_name,
        PL4PY.file2clob(D.DIRECTORY_NAME,  A.FUNC_NAME) CONTENTS,
-       D.DIRECTORY_PATH || '\'  || A.FUNC_NAME FULL_DIR,
+       D.DIRECTORY_PATH || case when PL4PY.get_os_type='windows' then '\' else '/' end  || A.FUNC_NAME FULL_DIR,
        A.last_update_datetime
   from pl4py_func_list a, dba_directories d
  where d.DIRECTORY_NAME = 'PY_FILE';
+
